86564ffe0a2795c05cf71d67de0d871a407857f5
gustavodgoias <gustavodgoias@gmail.com>
2026-05-21 08:33:05 -0300

    fix: remover produtos de 4kg e 20kg das paginas Home e Areias, manter apenas no Catalogo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -109,22 +109,6 @@ const areiasAssets = [
     btnHover: "#d65a10",
   },
   {
-    id: "tradicional-4kg",
-    image: "/images/produtos/AMIGO-MEU-4KG-TRADICIONAL.png",
-    accentColor: "#F37021",
-    accentLight: "#ff8c42",
-    btnBg: "#F37021",
-    btnHover: "#d65a10",
-  },
-  {
-    id: "tradicional-20kg",
-    image: "/images/produtos/AMIGO-MEU-20KG-2026.png",
-    accentColor: "#F37021",
-    accentLight: "#ff8c42",
-    btnBg: "#F37021",
-    btnHover: "#d65a10",
-  },
-  {
     id: "capim-limao",
     image: "/images/produtos/AMIGO-MEU-2KG-2025-CAPIM-LIMAO.png",
     accentColor: "#90C63E",
